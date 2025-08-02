cc1a5322c60eeb868973a977e1d29b3801d7134b
hanifa tavassoli <hanifatavassoli@gmail.com>
2025-08-02 23:56:14 +0430

    add script of home page

---
+++ script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+  function greeting() {
+    document.getElementById("greeting").textContent = "Welcome, Hanifa!";
+  }
+
+  greeting();
+});
